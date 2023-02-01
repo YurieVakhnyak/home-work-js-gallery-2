e76d50780f81a31a9f074981061e41bf7353a833
YVakhnyak <namejfk@gmail.com>
2023-02-01 15:36:25 +0200

    second task

---
+++ src/02-video.js
@@ -0,0 +1,51 @@
+// require(["https://player.vimeo.com/api/player.js"], function (Player) {
+//   const iframe = document.querySelector("#vimeo-video");
+//   const player = new Player(iframe);
+
+//   player.on("play", function () {
+//     console.log("played the video!");
+//   });
+// });
+import Player from "@vimeo/player";
+
+const iframe = document.querySelector("#vimeo-player");
+
+const player = new Player(iframe);
+player.on("play", function () {
+  console.log("played the video!");
+});
+
+player.on("timeupdate", function (seconds) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+  console.log(seconds);
+});
+
+let timeFromStorage = localStorage.getItem("videoplayer-current-time");
+const savedTime = JSON.parse(timeFromStorage);
+window.onload = function () {
+  let time = timeFromStorage;
+  if (time) {
+    console.log(`I'm time from Local Storage: ${timeFromStorage}`);
+    console.log(`I'm time from Local Storage: ${savedTime.seconds}`);
+    console.log(`It's length if Local Storage: ${localStorage.length}`);
+    player
+      .setCurrentTime(savedTime.seconds)
+      .then(function (seconds) {
+        seconds = time;
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case "RangeError":
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
+};
+
+const konjaka = "<h1>Igo-goshechki!!! tpruuu!!! </h1>";
+iframe.insertAdjacentHTML("afterend", konjaka);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, "./src/01-gallery.js"),
+    main: path.resolve(__dirname, "./src/02-video.js"),
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
@@ -11,22 +11,23 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/01-gallery.html"), // template file
+      template: path.resolve(__dirname, "./src/02-video.html"), // template file
+      //   template: path.resolve(__dirname, "./src/02-video.html"), // template file
       filename: "index.html", // output file
     }),
   ],
   module: {
     rules: [
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
       // JavaScript
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
-      },
-      // CSS, PostCSS, and Sass
-      {
-        test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
     ],
   },
